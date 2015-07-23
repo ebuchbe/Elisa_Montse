97236dace9561615bee34f9b8b6877db151056ad
MontseTor <mon.torres.oliva@gmail.com>
2015-07-23 12:53:26 +0200

    added Swirl commands script

---
+++ Swirl_commands.R
@@ -0,0 +1,16 @@
+install.packages("swirl")
+library("swirl")
+
+#I can only install courses if I have administrator permissiosn
+#To install a course I do it from R prompt:
+
+install_from_swirl("R Programming")
+#This worked running R as administrator only
+swirl()
+1: R Programming
+
+#I have finished 1: Basic Building Blocks
+#To get it validated in the Coursera R Programming Course:
+Course ID: rprog-030
+Submission login (email): m.torres224@yahoo.es
+Submission password: asdgVKpAYe
